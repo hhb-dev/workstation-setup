154e9cb77e5326adefdc9787af88edc87eba6975
suzusuke <suzusuke0806@gmail.com>
2022-11-03 12:38:41 +0900

    added generate ssh-key script

---
+++ scripts/ssh-key.sh
@@ -0,0 +1,29 @@
+echo
+echo "================="
+echo "  ssh-key start  "
+echo "================="
+
+cd ~/.ssh || exit
+ssh-keygen -t rsa
+open .
+
+echo
+echo "----------------------------------"
+echo "  please action to the following  "
+echo "----------------------------------"
+echo
+echo "copy id_rsb.pub"
+echo "paste to ssh-key at github, gitlab, etc."
+echo "** example url **"
+echo "https://github.com/settings/keys"
+echo
+echo "----------------------------------"
+echo "  thank you                       "
+echo "----------------------------------"
+echo
+
+
+echo "================="
+echo "  ssh-key end    "
+echo "================="
+echo
--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@ cd scripts || exit
 source ./config-osx.sh
 source ./homebrew.sh
 source ./oh-my-zsh.sh
-source ./git
-source ./dev-utlities
-source ./node
+source ./git.sh
+source ./dev-utlities.sh
+source ./node.sh
+source ./ssh-key.sh
