fc9ba63ba6396e2f87ea1499e0c2c66e1a978c4d
suzusuke <suzusuke0806@gmail.com>
2023-01-19 11:11:34 +0900

    added dev-utilities application

--- scripts/dev-utilities.sh
+++ scripts/dev-utilities.sh
@@ -7,14 +7,17 @@ echo "installing common application"
 brew install rectangle
 brew install clipy
 brew install discord
+brew install zoom
 brew install pushplaylabs-sidekick
 brew install android-file-transfer
+brew install miro
+brew install notion
 mas install 1429033973 # RunCat
 
 echo "installing dev tool"
 brew install fig
 fig install --input-method
-brew install postman-agent
+brew install postman
 brew install postgresql
 brew services start postgresql
 mas install 1483172210 # Free Ruler
