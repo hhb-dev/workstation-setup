a9f8388598df58e5619839da38457c1bca2e1621
suzusuke <suzusuke0806@gmail.com>
2023-02-11 22:58:48 +0900

    added common-utilities packages

---
+++ scripts/common-utilities.sh
@@ -0,0 +1,30 @@
+echo
+echo "=========================="
+echo "  common-utilities start  "
+echo "=========================="
+
+echo "installing common application"
+brew install rectangle
+brew install clipy
+brew install discord
+brew install slack
+brew install zoom
+brew install pushplaylabs-sidekick
+brew install miro
+brew install notion
+brew install figma
+mas install 1429033973 # RunCat
+
+echo "installing dev tool"
+brew install fig
+fig install --input-method
+brew install postman
+brew install postgresql
+brew services start postgresql
+brew install --cask docker
+mas install 1483172210 # Free Ruler
+
+echo "=========================="
+echo "  common-utilities end    "
+echo "=========================="
+echo
--- scripts/dev-utilities.sh
+++ scripts/dev-utilities.sh
@@ -3,16 +3,6 @@ echo "======================"
 echo "  dev-utilities start  "
 echo "======================"
 
-echo "installing common application"
-brew install rectangle
-brew install clipy
-brew install discord
-brew install zoom
-brew install pushplaylabs-sidekick
-brew install miro
-brew install notion
-mas install 1429033973 # RunCat
-
 echo "installing dev tool"
 brew install fig
 fig install --input-method
