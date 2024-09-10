158de26099b15e9d4ecd06ce4fe652904a01eeb6
suzusuke <suzusuke0806@gmail.com>
2024-09-10 20:46:57 +0900

    add volta install script for node environment

---
+++ scripts/05_node.sh
@@ -0,0 +1,12 @@
+    echo
+    echo "============="
+    echo "  node start  "
+    echo "============="
+
+    curl https://get.volta.sh | bash
+    volta install node
+
+    echo "============="
+    echo "  node end    "
+    echo "============="
+    echo
