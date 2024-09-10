dd401c714d581e398ef523554d948e9268883fa9
suzusuke <suzusuke0806@gmail.com>
2024-09-10 20:48:08 +0900

    add volta install script for node environment

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ source ./01_config-osx.sh
 source ./02_homebrew.sh
 source ./03_oh-my-zsh.sh
 source ./04_git.sh
+source ./05_node.sh
 source ./11_common-utilities.sh
 source ./12_dev-utilities.sh
 source ./13_ide.sh
