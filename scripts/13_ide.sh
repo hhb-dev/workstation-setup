8efa4029c2f07810e08586986eff90ac24e3396f
suzusuke <suzusuke0806@gmail.com>
2024-11-26 17:18:22 +0900

    managing IntelliJ plugin configurations

--- scripts/13_ide.sh
+++ scripts/13_ide.sh
@@ -3,9 +3,23 @@ echo "============="
 echo "  ide start  "
 echo "============="
 
-echo "installing intellij"
+echo "installing vim"
 brew install vim
+
+echo "installing intellij"
 brew install intellij-idea
+
+echo "installing intellij plugins"
+# for grammar
+open -na "IntelliJ IDEA" --args installPlugins tanvd.grazi
+# for visibility
+open -na "IntelliJ IDEA" --args installPlugins com.github.catppuccin.jetbrains_icons
+# for security shift left
+open -na "IntelliJ IDEA" --args installPlugins io.snyk.snyk-intellij-plugin
+# for frontend formatter
+open -na "IntelliJ IDEA" --args installPlugins com.github.biomejs.intellijbiome
+# for kotlin linter
+open -na "IntelliJ IDEA" --args com.nbadal.ktlint
 
 echo "installing xcode"
 mas install 497799835 # Xcode
