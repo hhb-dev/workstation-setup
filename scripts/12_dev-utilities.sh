6b5d67c6e6f49de998599c4ad9c664fa24451f28
suzusuke <suzusuke0806@gmail.com>
2025-05-27 11:59:39 +0900

    added aws cli and rds connection tool

--- scripts/12_dev-utilities.sh
+++ scripts/12_dev-utilities.sh
@@ -7,6 +7,9 @@ echo "installing dev tool"
 brew install --cask docker
 mas install 1483172210 # Free Ruler
 
+brew install awscli
+brew install --cask session-manager-plugin
+
 echo "======================"
 echo "  dev-utilities end    "
 echo "======================"
